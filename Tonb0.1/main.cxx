c91b220b4ffbbd50e7341fe1e1676d5c5d4a014d
amir5200fx <aasoleimani86@gmail.com>
2019-02-18 01:46:10 -0600

    Hello Qt!

--- Tonb0.1/main.cxx
+++ Tonb0.1/main.cxx
@@ -6,32 +6,28 @@
 #include <OFstream.hxx>
 #include <Vessels_DispNo1.hxx>
 
+#include <QtWidgets\qapplication.h>
+#include <QtWidgets\qlabel.h>
+
 using namespace AutLib;
 
 
-int main()
+int main(int argc, char *argv[])
 {
-	/*Smart_Variable<scalar, DynViscosityUnitExps> real("dynamic viscosity of water", 0.001, UnitSystem_DynViscosity_NSecPerSquareMeter);
-
-	fileName name = "out.txt";
-	OFstream out(name);
-	out << real << endl;*/
-
-	DispNo1_BareHull patch;
+	/*DispNo1_BareHull patch;
 	patch.FwdFullness().Value() = 0.0;
 	patch.AftFullness().Value() = 0.0;
 	patch.Perform();
 	patch.Discrete();
-	patch.ExportToFile();
+	patch.ExportToFile();*/
 
-	PAUSE;
-	return 0;
+	QApplication app(argc, argv);
+	QLabel *label = new QLabel("Hello Qt!");
+	label->show();
+	return app.exec();
 
-	std::cout << Convertor::Pressure<UnitSystem_Pressure_atm, UnitSystem_Pressure_psi>::value << std::endl;
-	std::cout << ConvertorTables::Length[UnitSystem_Length_cm][UnitSystem_Length_km] << std::endl;
-	std::cout << ConvertorTables::Velocity[UnitSystem_Velocity_kn][UnitSystem_Velocity_mPerSec] << std::endl;
+	/*PAUSE;
+	return 0;*/
 
-	std::cout << ConvertUnit(UnitSystem_Acceleration_ftPerSecPerSec, UnitSystem_Acceleration_mPerSecPerSec) << std::endl;
-
-	system("pause");
+	
 }
